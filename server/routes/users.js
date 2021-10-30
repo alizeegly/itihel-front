6a1bdabf74a2f74ec700e3a2851383331e75ce8f
Alizée <agillouaye@icloud.com>
2021-10-30 22:52:01 +0200

    [API] Get LoggedIn User

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
-
-// Filename : user.js
-
 const express = require("express");
-const { check, validationResult} = require("express-validator");
+const {
+    check,
+    validationResult
+} = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
 const User = require("../models/Users/User");
@@ -14,7 +15,6 @@ const User = require("../models/Users/User");
  * @param - /signup
  * @description - User SignUp
  */
-
 router.post("/signup",
     [
         check("pseudo", "Please Enter a valid Pseudo")
@@ -90,65 +90,89 @@ router.post("/signup",
     }
 );
 
-
+/**
+ * @method - POST
+ * @param - /login
+ * @description - User SignIn
+ */
 router.post("/login",
     [
-      check("email", "Please enter a valid email").isEmail(),
-      check("password", "Please enter a valid password").isLength({
-        min: 6
-      })
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
     ],
     async (req, res) => {
-      const errors = validationResult(req);
-  
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          errors: errors.array()
-        });
-      }
-  
-      const { email, password } = req.body;
-      try {
-        let user = await User.findOne({
-          email
-        });
-        if (!user)
-          return res.status(400).json({
-            message: "User Not Exist"
-          });
-  
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch)
-          return res.status(400).json({
-            message: "Incorrect Password !"
-          });
-  
-        const payload = {
-          user: {
-            id: user.id
-          }
-        };
-  
-        jwt.sign(
-          payload,
-          "randomString",
-          {
-            expiresIn: 3600
-          },
-          (err, token) => {
-            if (err) throw err;
-            res.status(200).json({
-              token
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
             });
-          }
-        );
-      } catch (e) {
-        console.error(e);
-        res.status(500).json({
-          message: "Server Error"
-        });
-      }
+        }
+
+        const {
+            email,
+            password
+        } = req.body;
+        try {
+            let user = await User.findOne({
+                email
+            });
+            if (!user)
+                return res.status(400).json({
+                    message: "User Not Exist"
+                });
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch)
+                return res.status(400).json({
+                    message: "Incorrect Password !"
+                });
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                "randomString", {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({
+                message: "Server Error"
+            });
+        }
     }
 );
+
+/**
+ * @method - GET
+ * @description - Get LoggedIn User
+ * @param - /user/me
+ */
+router.get("/me", auth, async (req, res) => {
+    try {
+        // request.user is getting fetched from Middleware after token authentication
+        const user = await User.findById(req.user.id);
+        res.json(user);
+    } catch (e) {
+        res.send({
+            message: "Error in Fetching user"
+        });
+    }
+});
+
 
 module.exports = router;
