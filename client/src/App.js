d8a80c534558ddb378bca5f33e408706d710a67b
Alizée <agillouaye@icloud.com>
2021-11-27 11:57:43 +0100

    Test dossier client

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:8800")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
 
   render(){
     return (
       <div className="App">
         Salut les gars !
-        <p>{this.state.apiResponse}</p>
         <Router>
           <Routes>
             <Route path="/" exact />
