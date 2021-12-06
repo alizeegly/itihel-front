a1d73553c14bad92e0f94ce4723c17f7b5fcba50
MaelBourdeleix <mael.bourdeleix@outlook.fr>
2021-12-06 11:14:42 +0100

    update

--- client/src/pages/Courses/Courses.jsx
+++ client/src/pages/Courses/Courses.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
-import Container from '../../components/User/Container'
 import "./courses.scss"
 
 const Courses = () => {
     return (
+
         <Sidebar/>
     )
 }
