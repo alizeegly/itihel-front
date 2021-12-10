4c0f7281cc42b9f3d341790bd290fa7adda6220c
MaelBourdeleix <mael.bourdeleix@outlook.fr>
2021-12-10 10:52:25 +0100

    Update du composant + appel sur la page courses.jsx

---
+++ client/src/components/Course/course.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./course.scss";
+import styled from 'styled-components';
+
+
+const Course = () => {
+    return (
+        <div className="course">
+            <h1>Titre de la carte</h1>
+            <p>Voici la description de la carte</p>
+            <img src={""}></img>
+        </div>
+    )
+}
+
+export default Course
--- client/src/pages/Courses/Courses.jsx
+++ client/src/pages/Courses/Courses.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Container from '../../components/User/Container'
 import CreateCourse from './CreateCourse'
+import Course from '../../components/Course/course.js'
 import "./courses.scss"
 
 const Courses = () => {
     return (
         <>
             <CreateCourse/>
+            <Course/>
         </>
     )
 }
