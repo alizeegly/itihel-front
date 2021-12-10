ea9cba509b30e671e8c6336700424f00150b06e2
Alizée <agillouaye@icloud.com>
2021-12-10 09:50:50 +0100

    page 404

--- client/src/pages/Errors/NotFound.js
+++ client/src/pages/Errors/NotFound.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const NotFound = () => (
   <div>
     <h1>404 - Not Found!</h1>
-    <Link to="/">Go Home</Link>
+    <p><Link to="/">Go Home</Link></p>
+    <p><Link to="/login">Go Login</Link></p>
+    <p><Link to="/sign-up">Go Create an account</Link></p>
   </div>
 );
 
