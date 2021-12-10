de9ae34c5ad93108e5e7a2105541fecb6b23911c
MaelBourdeleix <mael.bourdeleix@outlook.fr>
2021-12-10 15:59:49 +0100

    Appel API des cours de l'utilisateur
    - Ajout des cours de l'utilisateur
    - mise à jour du css (en cours)

--- client/src/pages/Courses/Courses.jsx
+++ client/src/pages/Courses/Courses.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import Container from '../../components/User/Container'
+import React, { useEffect, useState } from 'react'
+import "./courses.scss"
 import CreateCourse from './CreateCourse'
 import Course from '../../components/Course/course.js'
-import "./courses.scss"
+import axios from 'axios'
+import { useNavigate, Link } from "react-router-dom"
+import { useSession } from  'react-use-session'
+import { useParams } from 'react-router'
+
+
+
 
 const Courses = () => {
+    const navigate = useNavigate()
+    const { session, saveJWT, clear } = useSession('itihel')
+    const [courses, setCourses] = useState({})
+    const { id } = useParams();
+
+    console.log(courses)
+    const getCourses = async () => {
+        try {
+            const courses = await axios.get("/api/users/"+session.user.id+"/courses")
+            setCourses(courses.data);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    useEffect(()=>{
+        getCourses()
+    }, [])
+
     return (
         <>
             <CreateCourse/>
-            <ul>
-            <Course title="titre" description="description"/>
-            <Course title="titre 2" description="description 2"/>
+             <ul>
+                {courses && courses.courses && courses.courses.map((user) => (
+                    <div key={user._id} className="user">
+                        <Course title={user.title} description={user.description}/>
+
+                    </div>
+                ))}
             </ul>
+
         </>
     )
 }
