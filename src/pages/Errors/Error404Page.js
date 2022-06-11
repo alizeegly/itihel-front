04a44b8f72633184996f5ff719b142ef0139b70a
Julien Da Costa <juliendacosta27@gmail.com>
2022-06-11 18:22:23 +0200

    buttons redirection

--- src/pages/Errors/Error404Page.js
+++ src/pages/Errors/Error404Page.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import "./NotFound.scss"
+import { Button } from '@mui/material'
 import lottie from 'lottie-web'
 import {Navbar} from '../../components';
 
@@ -23,7 +24,14 @@ function Error404Page() {
 
             <div>
                 <div className="container animation-404" ref={container} data-aos="fade-down"></div>
-                <div>Cette page n'existe pas</div>
+                <div className="redirection-404">
+                    <h3>Une erreur est survenue. Redirigez-vous.</h3>
+                    <div className="redirection-404-buttons">
+                        <Button color="primary" variant="contained" href="/">Accueil</Button>
+                        <Button color="primary" variant="contained" href="/register">Inscription</Button>
+                        <Button color="primary" variant="contained" href="/login">Connexion</Button>
+                    </div>
+                </div>
             </div>
         </>
     )
